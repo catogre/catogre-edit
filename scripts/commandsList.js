6cd7ca22bca1cf4c30d8dfb41b2d15035af76a12
GDUcrash <gducrash@gmail.com>
2020-03-26 16:52:52 +0200

    some more cool features
    1) Made the highlight algorithm look for the commands in commandsList.js
    2) Added autocommplete for quotes (")
    3) Change the tab length from 2 to 4 spaces

--- scripts/commandsList.js
+++ scripts/commandsList.js
@@ -1,4 +1,5 @@
 let commandsList = [
+    //events
     {
         command: 'whenFlagClicked',
         type: 'event',
@@ -7,6 +8,72 @@ let commandsList = [
         description: 'Triggers every time the user clicks on green flag.'
     },
     {
+        command: 'whenKeyPressed',
+        type: 'event',
+        category: 'events',
+        autocomplete: 'whenKeyPressed "space":',
+        description: 'Triggers when the user presses a specific key.'
+    },
+    {
+        command: 'whenSpriteClicked',
+        type: 'event',
+        category: 'events',
+        autocomplete: 'whenSpriteClicked:',
+        description: 'Triggers when the user clicks on this sprite.'
+    },
+    {
+        command: 'whenMessage',
+        type: 'event',
+        category: 'events',
+        autocomplete: 'whenMessage "message1":',
+        description: 'Triggers when this sprite recieves a specific message. To send a message, use "broadcast()" block'
+    },
+    //control
+    {
+        command: 'whenCloneStarts',
+        type: 'event',
+        category: 'control',
+        autocomplete: 'whenCloneStarts:',
+        description: 'Scripts with this event will be executed by clones of this sprite. Clones are copies of the sprite with own properties and local variables/lists.'
+    },
+    {
+        command: 'wait',
+        type: 'function',
+        category: 'control',
+        autocomplete: 'wait(1);',
+        description: 'Waits a certain amount of seconds before executing the code after.'
+    },
+    {
+        command: 'createClone',
+        type: 'function',
+        category: 'control',
+        autocomplete: 'createClone(sprite);',
+        description: 'Creates a clone of this or any other sprite. Clones are copies of the sprite with own properties and local variables/lists.'
+    },
+    {
+        command: 'forever',
+        type: 'container',
+        category: 'control',
+        autocomplete: 'forever(){\n\n}',
+        description: 'Repeats the code inside it forever.'
+    },
+    {
+        command: 'if',
+        type: 'container',
+        category: 'control',
+        autocomplete: 'if(){\n\n}',
+        description: 'Checks if the statement if true and if it is, executes the code inside it.'
+    },
+    {
+        command: 'else',
+        type: 'container',
+        category: 'control',
+        autocomplete: 'else(){\n\n}',
+        description: '-'
+    },
+
+    //motion
+    {
         command: 'goTo',
         type: 'function',
         category: 'motion',
@@ -14,10 +81,68 @@ let commandsList = [
         description: 'Places the sprite in a certain position on the stage (x from -240 to 240 and y from -180 to 180)'
     },
     {
+        command: 'turnCW',
+        type: 'function',
+        category: 'motion',
+        autocomplete: 'turnCW(15);',
+        description: 'Turns the sprite clockwise by a certain amount of degrees.'
+    },
+    {
+        command: 'setX',
+        type: 'function',
+        category: 'motion',
+        autocomplete: 'setX(0);',
+        description: 'Sets the X (horizontal) position of the sprite to a certain number.'
+    },
+    {
+        command: 'setY',
+        type: 'function',
+        category: 'motion',
+        autocomplete: 'setY(0);',
+        description: 'Sets the Y (vertical) position of the sprite to a certain number.'
+    },
+    {
+        command: 'changeX',
+        type: 'function',
+        category: 'motion',
+        autocomplete: 'changeX(10);',
+        description: 'Changes the X (horizontal) position of the sprite by a certain number.'
+    },
+    {
+        command: 'changeY',
+        type: 'function',
+        category: 'motion',
+        autocomplete: 'changeY(10);',
+        description: 'Changes the Y (vertical) position of the sprite by a certain number.'
+    },
+    //looks
+    {
         command: 'say',
         type: 'function',
         category: 'looks',
         autocomplete: 'say("");',
         description: 'Adds a speech ballon near the sprites where certain text can be displayed. To remove the ballon, simply type "" as an argument.'
-    }
+    },
+    {
+        command: 'sayFor',
+        type: 'function',
+        category: 'looks',
+        autocomplete: 'sayFor("", 2);',
+        description: 'Adds a speech ballon near the sprites (where certain text can be displayed) for some amount of seconds.'
+    },
+    //sensing
+    {
+        command: 'keyPressed',
+        type: 'reporter',
+        category: 'sensing',
+        autocomplete: 'keyPressed("space")',
+        description: 'Checks if a certain key is pressed. If so, returns true, otherwise false.'
+    },
+    {
+        command: 'mouseDown',
+        type: 'reporter',
+        category: 'sensing',
+        autocomplete: 'mouseDown()',
+        description: 'Checks if the mouse button is down. If so, returns true, otherwise false.'
+    },
 ]
