389b42a31cad8327219bfe45e53f209aca3b6144
Florrie <towerofnix@gmail.com>
2020-03-29 16:28:16 -0300

    Revert "Revert "Insert (inline) span, rather than (full line) div, when dragging reporters""
    This reverts commit 986b08ddeb990c2c9d735d3eece05d9e433efe7e.

--- scripts/smoothDrag.js
+++ scripts/smoothDrag.js
@@ -55,8 +55,7 @@ function setItemDraggable(item, easeFactor, rotFactor){
 }
 
 //smooth drag for code palette items
-
-function setItemPaletteDraggable(item, easeFactor, rotFactor, eDown){
+function setItemPaletteDraggable(item, content, category, insertType, easeFactor, rotFactor, eDown){
     let mousePos = {
         x: eDown.pageX,
         y: eDown.pageY
@@ -73,11 +72,18 @@ function setItemPaletteDraggable(item, easeFactor, rotFactor, eDown){
     let targetY;
 
     let targetLine;
-    let tempLine = document.createElement('div');
-    tempLine.style.display = 'none';
-    tempLine.classList.add('temp-line');
-    codeArea.appendChild(tempLine);
-    code.style.display = 'flex';
+    let tempLine;
+    let tempSpan;
+
+    if (insertType === 'LINE') {
+        tempLine = document.createElement('div');
+        tempLine.classList.add('line', 'temp-line', category);
+        tempLine.appendChild(document.createTextNode(content));
+    } else if (insertType === 'SPAN') {
+        tempSpan = document.createElement('span');
+        tempSpan.classList.add('temp-span', category);
+        tempSpan.appendChild(document.createTextNode(content));
+    }
 
     let overCodeArea = false;
 
@@ -95,34 +101,112 @@ function setItemPaletteDraggable(item, easeFactor, rotFactor, eDown){
         item.style.transform = `rotate(${itemDirection}rad)`;
     }, 10);
 
+    function insertAtColumn(insertColumn, lineEl, insertEl) {
+        let column = 0;
+        insertEl.remove();
+        function insertElIntoElementOrContinue(parent) {
+            let child;
+            for (child of parent.childNodes) {
+                if (child.nodeType === document.TEXT_NODE) {
+                    const nodeEndColumn = column + child.textContent.length;
+                    const nodeStartColumn = column;
+                    if (nodeStartColumn === insertColumn) {
+                        // Node STARTS AT insertion column.
+                        // Node may be left alone, with inserted element placed before.
+                        parent.insertBefore(insertEl, child);
+                        return true;
+                    } else if (nodeEndColumn > insertColumn) {
+                        // Node CONTAINS insertion column.
+                        // Node must be broken into two nodes, with inserted element placed between.
+                        const sliceColumn = insertColumn - nodeStartColumn;
+                        const firstText = child.textContent.slice(0, sliceColumn);
+                        const secondText = child.textContent.slice(sliceColumn);
+                        const firstNewNode = document.createTextNode(firstText);
+                        const secondNewNode = document.createTextNode(secondText);
+                        parent.insertBefore(firstNewNode, child);
+                        parent.insertBefore(insertEl, child);
+                        parent.insertBefore(secondNewNode, child);
+                        parent.removeChild(child);
+                        return true;
+                    } else if (nodeEndColumn === insertColumn) {
+                        // Node ENDS AT insertion column.
+                        // Node may be left alone, with inserted element placed after.
+                        if (child.nextSibling) {
+                            parent.insertBefore(insertEl, child.nextSibling);
+                        } else {
+                            parent.appendChlid(insertEl);
+                        }
+                        return true;
+                    } else {
+                        // Node ENDS BEFORE insertion column.
+                        // Carry on to the next element.
+                        column = nodeEndColumn;
+                    }
+                } else if (child.nodeType === document.ELEMENT_NODE) {
+                    if (insertElIntoElementOrContinue(child)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        if (!insertElIntoElementOrContinue(lineEl)) {
+            // Couldn't find the column to insert the element at.
+            // Either an error, or the insertion column is past the end of the line.
+            // Assume the latter and append the element to the end of the line.
+            lineEl.appendChild(insertEl);
+        }
+    }
+
     inputArea.onmouseover = () => overCodeArea = true;
     inputArea.onmouseout = () => overCodeArea = false;
 
     document.onmousemove = function(e){
+        if (insertType === 'LINE') {
+            tempLine.remove();
+        } else if (insertType === 'SPAN') {
+            tempSpan.remove();
+        }
+
         mousePos = {
             x: e.pageX,
             y: e.pageY
         }
-        targetLine = Math.round((itemTop - 60 + inputArea.scrollTop)/20);
-        tempLine.style.order = (targetLine * 2) + 1;
 
         let codeAreaRect = codeArea.getBoundingClientRect();
         let rectMatchX = mousePos.x > codeAreaRect.x && mousePos.x < codeAreaRect.width+codeAreaRect.x;
         let rectMatchY = mousePos.y > codeAreaRect.y-10 && mousePos.y < codeAreaRect.height+codeAreaRect.y;
+        if (!(rectMatchX && rectMatchY)) return;
 
-        if(rectMatchX && rectMatchY) overCodeArea = true;
-        else overCodeArea = false;
+        targetLine = Math.round((itemTop - codeAreaRect.top + inputArea.scrollTop)/20);
+        if (targetLine < 1) return;
+        if (targetLine > codeArea.children.length + 1) return;
 
-        if(targetLine > codeArea.children.length) overCodeArea = false;
-
-        tempLine.style.display = 'none';
-        if(overCodeArea) tempLine.style.display = 'block';
+        const targetLineEl = codeArea.children[targetLine - 1];
+        if (insertType === 'LINE') {
+            if (targetLineEl) {
+                codeArea.insertBefore(tempLine, targetLineEl);
+            } else {
+                codeArea.appendChild(tempLine);
+            }
+        } else if (insertType === 'SPAN') {
+            if (targetLineEl) {
+                console.log(targetLineEl);
+                // TODO: calculate good insertion points, choose the closest one to cursor
+                const insertColumn = 0;
+                insertAtColumn(insertColumn, targetLineEl, tempSpan);
+            }
+        }
     }
 
     document.onmouseup = function(){
         clearInterval(interval);
         code.style.display = 'block';
-        codeArea.removeChild(tempLine);
+        if (insertType === 'LINE') {
+            tempLine.remove();
+        } else if (insertType === 'SPAN') {
+            tempSpan.remove();
+        }
         item.style.transform = null;
         document.onmousemove = null;
         document.onmouseup = null;
